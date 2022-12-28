e2c01ce4f6b0bf009bb6f15f6ceb03c5ab30f11b
eleahmloucks <eleahloucks@gmail.com>
2022-12-28 13:12:18 -0800

    added more practice

--- python.py
+++ python.py
@@ -18,3 +18,47 @@ class Solution:
             else:
                 return index
         return -1
+
+"""Python functions for JavaScript Trials 1."""
+
+
+def output_all_items(items):
+    for item in items:
+        print(item)
+
+
+
+def get_all_evens(nums):
+    for num in nums:
+        if num % 2 == 0:
+            return num
+
+
+def get_odd_indices(items):
+    result =[]
+    for i in range(len(items)):
+        if i % 2 != 0:
+            result.append(i)
+    return result
+
+
+def print_as_numbered_list(items):
+    i = 1
+    for item in items:
+        print(f'{i}. {item}')
+        i += 1
+
+
+
+
+def get_range(start, stop):
+    nums=[]
+
+    for i in range(start, stop):
+        nums.append(i)
+
+    return nums
+
+
+
+
